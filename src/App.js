c827238e7de2132345f9fe963aff8277bec2b78f
Adewale Abati <acekyd01@gmail.com>
2019-04-23 16:30:16 +0100

    Initial updates

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Authenticate with GitHub
           </a>
         </header>
       </div>
