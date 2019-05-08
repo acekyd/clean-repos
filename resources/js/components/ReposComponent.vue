fa52680b2cff9423be585ff8406a0f1c90ed91d6
Adewale Abati <acekyd01@gmail.com>
2019-05-08 19:00:25 +0100

    VueScrollbar set uo

--- resources/js/components/ReposComponent.vue
+++ resources/js/components/ReposComponent.vue
@@ -7,12 +7,10 @@
                     <p>Repos have been sorted for those likely to want to be deleted.</p>
                 </div>
 
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
+                <div class="repos">
+                    <vue-custom-scrollbar class="scroll-area" :settings="settings">
+                        <img src="http://utatti.github.io/perfect-scrollbar/azusa.jpg" height="720" width="1280" alt="">
+                    </vue-custom-scrollbar>
                 </div>
             </div>
         </div>
@@ -20,6 +18,9 @@
 </template>
 
 <script>
+    // import installed component for scrollbar
+    import vueCustomScrollbar from 'vue-custom-scrollbar'
+
     export default {
         props: {
             user: {
@@ -27,9 +28,35 @@
                 required: true
             }
         },
+        components: {
+            vueCustomScrollbar
+        },
+        data() {
+            return {
+                settings: {
+                    maxScrollbarLength: 60,
+                    suppressScrollX: true
+                }
+            }
+        },
+        methods: {
+        },
         mounted() {
             console.log('Component mounted.')
             console.log(this.user);
         }
     }
 </script>
+<style >
+.scroll-area {
+  position: relative;
+  margin: auto;
+  height: 500px;
+}
+.ps__thumb-y, .ps__rail-y:hover>.ps__thumb-y {
+    background-color: #09be67;
+}
+.ps__rail-y, .ps__rail-y:hover, .ps__rail-y.ps--clicking .ps__thumb-y, .ps .ps__rail-y:hover {
+    background-color: rgba(255, 255, 255, 0.11);
+}
+</style>
